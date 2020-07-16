106af1f32fae0b6ba1951046df82195bfeb1991b
Andrei Fursin <fursin@gmail.com>
2020-07-16 10:14:37 +0300

    Updates

---
+++ precache-manifest.e1d0439f125e67dc920564b24c90bb2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd33fa18dcc21375ed5dc7359b9b81ab",
+    "url": "/andrewfu/3dui/index.html"
+  },
+  {
+    "revision": "49af2d20d7df688402a6",
+    "url": "/andrewfu/3dui/static/js/2.4507ff91.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/andrewfu/3dui/static/js/2.4507ff91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bbc5b08b4067a8b0553",
+    "url": "/andrewfu/3dui/static/js/main.561e1151.chunk.js"
+  },
+  {
+    "revision": "783f055ff24c5c48843f",
+    "url": "/andrewfu/3dui/static/js/runtime-main.08fec15b.js"
+  }
+]);
